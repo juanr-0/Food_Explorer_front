408306c4c680f4f1d61ec19c25847ff67c6f28ac
juan rodrigues <eng.juan.rodrigues@gmail.com>
2023-11-02 17:19:57 -0300

    mudnaça de baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://api-foodexplorer-58lk.onrender.com',
   withCredentials:true
 });
